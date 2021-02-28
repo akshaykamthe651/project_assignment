3849d57c5fee01c67e66c580dfdf3e55f06d490d
akshaykamthe651 <59759952+akshaykamthe651@users.noreply.github.com>
2021-03-01 00:09:56 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,38 +1,40 @@
-const { fstat } = require('fs');
-const { request } = require('http');
-var url=require('url');
-var fs=require('fs');
+// var url=require('url');
+// var fs=require('fs');
 
-function renderHTML(path,response){
+// function renderHTML(path,response){
 
-    fs.readFile(path,null,function(error, data){
-        if(error){
-            response.writeHead(404);
-            response.write('File not found');
-        } else{
-            response.write(data);
-        }
-        response.end();
-    });
-}
+//     fs.readFile(path,null,function(error, data){
+//         if(error){
+//             response.writeHead(404);
+//             response.write('File not found');
+//         } else{
+//             response.write(data);
+//         }
+//         response.end();
+//     });
+// }
 
-module.exports = {
-handleRequest: function(request, response){
-    response.writeHead(200,{ 'Content-Type':'text/html'});
+// module.exports = {
+// handleRequest: function(request, response){
+//     response.writeHead(200,{ "Content-Type":"text/html"});
 
-    var path=url.parse(request.url).pathname;
+//     var path=url.parse(request.url).pathname;
 
-    switch (path){
-        case'/':
-            renderHTML('./index.html',response);
-            break;
-        case '/hotel/room-type':
-            renderHTML('./index1.html',response);
-        default :
-            response.writeHead(404);
-            response.write('route not defined');
-            response.end();
+//     switch (path){
+//         case'/':
+//             renderHTML('./index.html',response);
+//             break;
+//         case '/hotel':
+//             renderHTML('./index1.html',response);
+//             break;
+//         default :
+//             response.writeHead(404);
+//             response.write('route not defined');
+//             response.end();
 
-        }
-    }
-};
+//         }
+//     }
+// };
+
+
+
--- server.js
+++ server.js
@@ -1,6 +1,96 @@
-var http=require('http');
-var app=require('./app');
+// const http=require('http');
+// var app=require('./app');
 
-http.createServer(app.handleRequest).listen(1337, "127.0.0.1");
+// http.createServer(app.handleRequest).listen(1337, "127.0.0.1", () => {
 
-console.log('Server running at http://127.0.0.1:1337/');
+// console.log('Server running at http://127.0.0.1:1337/');
+// });
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.get('/', (req, res) => {
+//     console.log('Got body:', req.body);
+// var data=res.send;
+// console.log(data);
+//     res.send('hello world');
+// });
+// app.get();
+
+app.listen(8080, () => console.log(`Started server at http://localhost:8080`));
+
+// const request=require('request')
+// request.get("http://localhost:3000/hotels",(error,req,body)=>{
+
+// if(error){
+//     return(  console.log("error"))
+      
+// }
+// console.dir(JSON.parse(body));
+// })
+
+
+
+// var express = require('express');
+// var app = express();
+
+
+// app.get('http://localhost:3000/hotels', function (req, res) {
+//    // First read existing users.
+ 
+//       var users = JSON.parse( data );
+//       var user = users["user" + req.params.id] 
+//       console.log( user );
+//       res.end( JSON.stringify(user));
+ 
+// })
+
+// var server = app.listen(8081, function () {
+//    var host = server.address().address
+//    var port = server.address().port
+//    console.log("Example app listening at http://%s:%s", host, port)
+// })
+
+
+var fs=require("fs");
+var Request = require("request");
+
+Request.get("http://localhost:3000/hotels", (error, response, body) => {
+    if(error) {
+        return console.dir(error);
+    }
+    //let data = JSON.stringify(body);
+    fs.writeFileSync('demo.json', body);
+   // response.status(200).send(result);
+    console.dir(JSON.parse(body));
+});
+
+fs.readFile('demo.json', 'utf8' , (err, data) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+
+    console.log('reading from file');
+    console.log(data);
+    var test = require('./demo.json');
+    console.log("************");
+    console.log(test);
+    console.log("************");
+
+    var afterFilter=test.filter((room-type)=> test.room-type =="Large");
+ 
+    console.log("************before filter");
+    console.log(afterFilter);
+    console.log("************after filter");
+  });
+
+Request.get("http://localhost:8000/hotels", (error, response, body) => {
+    if(error) {
+        return console.dir(error);
+    }
+    console.dir(JSON.parse(body));
+});
