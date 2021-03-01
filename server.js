3d16997d90ffbedbec1ec17cea6ed0ca9b0cdd13
akshaykamthe651 <59759952+akshaykamthe651@users.noreply.github.com>
2021-03-01 19:10:27 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,96 +1,86 @@
-// const http=require('http');
-// var app=require('./app');
-
-// http.createServer(app.handleRequest).listen(1337, "127.0.0.1", () => {
-
-// console.log('Server running at http://127.0.0.1:1337/');
-// });
-
 const express = require('express');
 const bodyParser = require('body-parser');
-
-const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// app.get('/', (req, res) => {
-//     console.log('Got body:', req.body);
-// var data=res.send;
-// console.log(data);
-//     res.send('hello world');
-// });
-// app.get();
-
-app.listen(8080, () => console.log(`Started server at http://localhost:8080`));
-
-// const request=require('request')
-// request.get("http://localhost:3000/hotels",(error,req,body)=>{
-
-// if(error){
-//     return(  console.log("error"))
-      
-// }
-// console.dir(JSON.parse(body));
-// })
-
-
-
-// var express = require('express');
-// var app = express();
-
-
-// app.get('http://localhost:3000/hotels', function (req, res) {
-//    // First read existing users.
- 
-//       var users = JSON.parse( data );
-//       var user = users["user" + req.params.id] 
-//       console.log( user );
-//       res.end( JSON.stringify(user));
- 
-// })
-
-// var server = app.listen(8081, function () {
-//    var host = server.address().address
-//    var port = server.address().port
-//    console.log("Example app listening at http://%s:%s", host, port)
-// })
-
-
 var fs=require("fs");
 var Request = require("request");
+var url=require('url');
+//const { response } = require('express');
+var _ = require("underscore");
+var firstresult;
+const app = express();
+var allData;
 
-Request.get("http://localhost:3000/hotels", (error, response, body) => {
-    if(error) {
-        return console.dir(error);
-    }
-    //let data = JSON.stringify(body);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.listen(8080, () => console.log(`Started server at http://localhost:8080`));
+
+
+app.get("/hotels",(req,resp)=>{
+    var test
+    Request.get("http://localhost:7000/hotels", (error, response, body) => {
+        if (!error && response.statusCode == 200)
     fs.writeFileSync('demo.json', body);
-   // response.status(200).send(result);
+    firstresult= JSON.parse(body);
     console.dir(JSON.parse(body));
+    callfunction(); //call for 2nd REST API
+    
 });
+function callfunction()
+{
+    console.log("hii");
+    Request.get("http://localhost:3000/hotels", (error, response, body) => {
+        if (!error && response.statusCode == 200)
+    console.log("body"+body)
+    console.log("in 2nd api call")
+    var json = JSON.parse(body)
+    firstresult.push(json)
+   
+    fs.writeFile('demo.json', JSON.stringify(firstresult),function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+      });
+});
+}
 
 fs.readFile('demo.json', 'utf8' , (err, data) => {
     if (err) {
       console.error(err)
       return
     }
-
-    console.log('reading from file');
-    console.log(data);
-    var test = require('./demo.json');
+     console.log('reading from file');
+    // console.log(data);
+   // test = data
+   allData=data
     console.log("************");
-    console.log(test);
+    console.log("Test file"+allData);
     console.log("************");
-
-    var afterFilter=test.filter((room-type)=> test.room-type =="Large");
- 
-    console.log("************before filter");
-    console.log(afterFilter);
-    console.log("************after filter");
+    resp.send(allData)
   });
 
-Request.get("http://localhost:8000/hotels", (error, response, body) => {
-    if(error) {
-        return console.dir(error);
-    }
-    console.dir(JSON.parse(body));
+  });
+
+  //Filter Condition on roomType and availiability
+  app.get("/hotels/:roomType",(req,res)=>{
+
+    console.log("inside roomType");
+	var roomType=req.params.roomType;
+    console.log(roomType);
+    
+    fs.readFile('demo.json', 'utf8' , (err, data) => {
+        if (err) {
+          console.error(err)
+          return
+        } 
+       console.log(data);
+       allData=JSON.parse(data)
+        console.log(allData);                  //filter function condition on  roomType & Availability
+myArray = allData.filter(allData =>                         
+    (allData.roomType==roomType && allData.availability>0 ) ); 
+console.log(myArray);                                          
+res.send(myArray);
+
 });
+    })
+    
+	
+
+  
