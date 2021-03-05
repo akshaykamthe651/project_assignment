01ab2086fd9132b838e379ba7b8984a3816a15a2
akshaykamthe651 <59759952+akshaykamthe651@users.noreply.github.com>
2021-03-05 16:37:09 +0530

    Add files via upload

---
+++ apicall.js
@@ -0,0 +1,33 @@
+// const axios=require('axios')
+
+
+// axios.all([
+    
+//     axios.get('http://localhost:3000/hotels'),
+//     axios.get('http://localhost:7000/hotels')
+//         ])
+        
+//         .then(responseArr => {
+//             var allData;
+//             console.log("firstAPI data",responseArr[0].data);
+//             console.log("secondAPI data",responseArr[1].data);
+//             firstResult=responseArr[0].data
+//             secondResult=responseArr[1].data
+//             console.log("finalArray"+allData);
+//             return firstResult
+//         })
+//         .catch(err => { 
+//             console.log(err,err.response);
+//         });
+  
+ 
+
+// axios.put('http://localhost:3000/hotels',{
+//         id : 1,
+//         availability : 5
+//     })
+//     .then(responseArr=> console.log(responseArr)) 
+//     .catch(err =>console.log(err))
+
+   
+
--- server.js
+++ server.js
@@ -5,55 +5,84 @@ var Request = require("request");
 var url = require('url');
 var _ = require("underscore");
 const { response } = require('express');
-var firstResult;
-var secondResult;
+var firstParamter;
+var secondParamter;
 var allData;
-const app = express();
-const axios = require('axios').default;
 
+const app = express();
+const axios = require('axios');
+const { json } = require('body-parser');
+
+var one="http://localhost:3000/hotels";
+var two="http://localhost:7000/hotels";
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.listen(8080, () => console.log(`Started server at http://localhost:8080`)); 
+app.listen(8080, () => console.log(`Started server at http://localhost:8080`));
 
 //Initial API call to get data from Both sources
 
 app.get("/hotels", (req, resp) => {
-    var test
-    Request.get("http://localhost:7000/hotels", (error, response, body) => {
-        if (!error && response.statusCode == 200)
-            firstResult = JSON.parse(body);
-        console.dir(JSON.parse(body));
-        callfunction();                         //call for 2nd REST API
-    });
-    function callfunction() {
 
-        Request.get("http://localhost:3000/hotels", (error, response, body) => {
-            if (!error && response.statusCode == 200)
+    if (req.query.roomType === undefined) {
+    const requestOne=axios.get(one);
+    const requestTwo=axios.get(two);
 
-                secondResult = JSON.parse(body);
-            console.dir(secondResult);
-            console.dir(JSON.parse(body));
-            allData = firstResult.concat(secondResult);   //concated data from 2 API'S
+    console.log(req.query.roomType);
 
-            fs.writeFile('demo.json', JSON.stringify(allData), function (err) {
-                if (err) throw err;                        //Write operation on combined data from 2 API'S                        
+    axios.all([requestOne, requestTwo])
+        .then(axios.spread((...responses) => {
+         responseOne =responses[0].data;
+         responseTwo = responses[1].data;
+
+         allData = responseOne.concat(responseTwo);
+         console.log("finalArray" + allData);
+         resp.send(allData);
+         }))
+        .catch(err => {
+                console.log(err, err.response);
             });
-        });
+
     }
+    else if (req.query.roomType && req.query.roomType) {
+        console.log("inside room filter");
+        
+        console.log(firstParamter = req.query.roomType[0]);
+        console.log(secondParamter = req.query.roomType[1]);
 
-    fs.readFile('demo.json', 'utf8', (err, data) => {
-        //Reading data from main source
-        if (err) {
-            console.error(err)
-            return
-        }
-        allData = data
-        resp.send(allData)
-    });
-
-});
-
+        const requestOne=axios.get(one);
+        const requestTwo=axios.get(two);  
     
+        axios.all([requestOne, requestTwo])
+            .then(axios.spread((...responses) => {
+             responseOne =responses[0].data;
+             responseTwo = responses[1].data;
+    
+             allData = responseOne.concat(responseTwo); //filter function condition on  roomType & Availability
+             console.log("finalArray" +(JSON.stringify(allData)));
+            var roomSource =allData.filter(allData => (allData.roomType == firstParamter && allData.roomType == secondParamter));
+            console.log("*******"+roomSource+"filtered data");
+             resp.send(roomSource);
+             }))
+                // console.log("finalArray" + JSON.stringify(allData));
+                // dataForFilter=JSON.parse(allData)
+               
+            .catch(err => {
+                console.log(err, err.response);
+            });
+
+        }
+    });
+    //     else if(req.query.charges!=={})
+    //     {
+    //         console.log("inside charges And roomType Filter");
+    //         console.log(firstParamter = req.query.charges);
+    //         console.log(secondParameter=req.query.roomType);
+           
+    //         resp.send("3rd api");
+    //     }
+        
+    // });
+
 // Filter Condition on roomType and availiability
 app.get("/hotels/:roomType", (req, res) => {
 
@@ -84,11 +113,11 @@ app.get("/searchByName/:name", (req, res) => {
             return
         }
         allData = JSON.parse(data);
-                
-    myArray = allData.filter(allData =>     //filter condition on  ByHotelname
-        (allData.name == hotelName));
-    console.log(myArray);
-    res.send(myArray);
+
+        myArray = allData.filter(allData =>     //filter condition on  ByHotelname
+            (allData.name == hotelName));
+        console.log(myArray);
+        res.send(myArray);
     })
 });
 
@@ -105,11 +134,11 @@ app.get("/hotelsRoomCharge/:charges", (req, res) => {
             return
         }
         allData = JSON.parse(data);
-                  
-    myArray = allData.filter(allData =>          //filter function condition on  Hotel charges range
-        (allData.charges <= Charges || Charges == allData.charges));
 
-    res.send(myArray);
+        myArray = allData.filter(allData =>          //filter function condition on  Hotel charges range
+            (allData.charges <= Charges || Charges == allData.charges));
+
+        res.send(myArray);
 
     })
 });
